a37fa129b41655ad85045c9f015aafbbfe8b21df
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-09-29 12:18:51 -0500

    3.4 Estructurando evaluaciones, Conoce y usa el método Replace en las cadenas de texto

---
+++ 003 Estructurando evaluaciones/3.4 Conoce y usa el método Replace en las cadenas de texto/app.js
@@ -0,0 +1,7 @@
+/**
+ * el modificador g
+ * [mensaje /expresion/g sirve para hacer busqueda y cambios globales]
+ * @type {String}
+ */
+var mensaje = 'El cliente pidió sushi y comió su sushi';
+console.log(mensaje.replace(/sushi/g, 'tacos'));
